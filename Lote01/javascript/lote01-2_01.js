93c986009af7f4fe9bd82888ae5f281699d544d4
William Villela de Carvalho <william.carvalho12@fatec.sp.gov.br>
2018-11-05 03:17:40 -0200

    lote01_2 correcao

--- Lote01/javascript/lote01-2_01.js
+++ Lote01/javascript/lote01-2_01.js
@@ -1,22 +1,32 @@
 vt = new Array(49) 
+var x = 0;
+var md = 0;
+var soma_imp = 0;
+
 for (i=0; i<=49; i++){
-
-    vt[i] = parseInt(prompt("Insira um Numero")); 
-
+    console.log("i: ", i);
+    vt[i] = parseFloat(prompt("Insira um Numero"));  
+    console.log(vt[i]);
+ 
     if (vt[i] >=10 && vt[i] <=200){
 
-        var x++;
-        var md=md+vt[i];
+        x++;
+        console.log("X: ", x);
+        md = parseFloat(md) + parseFloat(vt[i]);
+        console.log("MD:",md);
 
     }
 
-    if (vt[i] % 2 == 1){
-        var soma_imp = soma_imp+vt[i];
+    if ( vt[i] % 2 == 1){
+        soma_imp = soma_imp + vt[i];
+        console.log("Soma_imp:",soma_imp);
+
     }
 
 
 }
-var md = md / x;
-console.log(md);
-console.log(soma_imp);
+md = md / x;
 
+console.log("Media: ", md);
+
+console.log("Soma dos Impares: ", soma_imp);
