b70e95aecb8b481ff7f485a86e3d77009f8cfdd1
Bruno-Pallin <43732623+Bruno-Pallin@users.noreply.github.com>
2019-02-26 22:41:31 -0300

    Exercicios ate o 17

---
+++ Lote01/Java/Prj_Lot0107022019/src/exercicios_lote01/Lt01_EstSeqEx05.java
@@ -0,0 +1,31 @@
+/* 5.	Receba os coeficientes A, B e C de uma equação do 2º grau (AX²+BX+C=0). Calcule e mostre as raízes reais (considerar que a 
+ equação possue 2 raízes).
+ ****************************************
+ Objetivo: Quinto exercício em java
+ Programador: Bruno Pallin
+ Data da Criação: 12/02/2019 
+ ****************************************
+ */
+
+package exercicios_lote01;
+import javax.swing.JOptionPane;
+
+public class Lt01_EstSeqEx05 {
+	public static void main (String args[]) {
+		double a, b, c, delta, r1, r2;
+		String raizes;
+		
+		a = Integer.parseInt(JOptionPane.showInputDialog("Insira o valor de A da equação de 2º grau:")); //2
+		b = Integer.parseInt(JOptionPane.showInputDialog("Insira o valor de B da equação de 2º grau:")); //5
+		c = Integer.parseInt(JOptionPane.showInputDialog("Insira o valor de C da equação de 2º grau:")); // 2
+
+		delta = (b*b) - (4*a*c); //9
+		
+		r1 = (-b + Math.sqrt(delta))/(2*a);
+		r2 = (-b - Math.sqrt(delta))/(2*a);				
+	
+		raizes = "A raizes são:\n" + "R1: " + r1 + "\nR2: " + r2;
+		JOptionPane.showMessageDialog(null, raizes);
+	}
+
+}
---
+++ Lote01/Java/Prj_Lot0107022019/src/exercicios_lote01/Lt01_EstSeqEx10.java
@@ -0,0 +1,23 @@
+/* 10.	Receba 2 números reais. Calcule e mostre a diferença desses valores.
+ ****************************************
+ Objetivo: Décimo exercício em java
+ Programador: Bruno Pallin
+ Data da Criação: 13/02/2019 
+ ****************************************
+ */
+
+package exercicios_lote01;
+import javax.swing.JOptionPane;
+
+public class Lt01_EstSeqEx10 {
+	public static void main (String args[]) {
+		double num1, num2, diferenca ;
+		
+		num1 = Double.parseDouble(JOptionPane.showInputDialog("Insira o primeiro valor:"));
+		num2 = Double.parseDouble(JOptionPane.showInputDialog("Insira o segundo valor:"));
+		
+		diferenca = num1 - num2;
+		
+		JOptionPane.showMessageDialog(null, "A diferença desses valores é de: " + diferenca);
+	}
+}
