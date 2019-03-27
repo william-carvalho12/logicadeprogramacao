fc38c4c73c0cd24107dd05c9e161452362acd052
Bruno-Pallin <43732623+Bruno-Pallin@users.noreply.github.com>
2019-03-27 11:03:12 -0300

    Lote Recursivo 5

--- Lote01/Java/Prj_Lot0107022019/src/recursividade/Recursividade05.java
+++ Lote01/Java/Prj_Lot0107022019/src/recursividade/Recursividade05.java
@@ -1,6 +1,40 @@
 // 5) Serie5 = (N)! + (N-1)! + (N-2)! + ... + (1)!
 package recursividade;
 import javax.swing.JOptionPane;
-public class Recursividade05 {
+public class Recursividade05 {	
+	public static void main(String[] args) {
+		int entrada, resultadoSerie;
+		
+		entrada = Integer.parseInt(JOptionPane.showInputDialog("Insira o valor da série 5"));
+		
+		resultadoSerie = serie5(entrada);
+		
+		JOptionPane.showMessageDialog(null, resultadoSerie);
+		
+	}
+	
+	static int serie5(int n) {
+		int serie;
+		if (n>1) {
+			serie = Fat(n) + serie5(n-1);
+			return serie;
+		} else {
+			return n;
+		}
+	
+	}
+	
+	static int Fat(int n) {
+		int fatorial = 1;;
+		
+		if(n>1) {
+			fatorial = n * Fat(n-1);
+			return fatorial;
+		} else {
+			return n;
+		}
+	}
+	
+
 
 }
